b6f066d7e83ebfa04c0dc6bd51b03b048bb532de
SADfranco <76488291+SADfranco@users.noreply.github.com>
2022-01-14 13:57:50 +0000

    squarefigure

--- squarefigure.py
+++ squarefigure.py
@@ -1,7 +1,15 @@
 figure = str(input())
-a = int(input())
-b = int(input())
-c = int(input())
-
 if figure == 'треугольник':
-    print(a * b * c)
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    p = (a + b + c) / 2
+    S = (p * (p - a) * (p - b) * (p - c))**(0.5)
+    print(float(S))
+elif figure == 'прямоугольник':
+    a = int(input())
+    b = int(input())
+    print(a * b)   
+elif figure == 'круг':
+    a = int(input())
+    print(3.14 * a ** 2)
