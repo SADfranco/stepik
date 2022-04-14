262ac55db9dfacbb4556e6e56753a799aa719574
SADfranco <76488291+SADfranco@users.noreply.github.com>
2022-04-14 15:15:28 +0000

    new tasks

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-figure = str(input())
-if figure == 'треугольник':
-    a = int(input())
-    b = int(input())
-    c = int(input())
-    p = (a + b + c) / 2
-    S = (p * (p - a) * (p - b) * (p - c))**(0.5)
-    print(float(S))
-elif figure == 'прямоугольник':
-    a = int(input())
-    b = int(input())
-    print(a * b)   
-elif figure == 'круг':
-    a = int(input())
-    print(3.14 * a ** 2)
+import color
