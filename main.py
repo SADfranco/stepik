854f0e2d8edf9bd1e459670256b983c643dfd752
SADfranco <76488291+SADfranco@users.noreply.github.com>
2022-05-12 13:00:28 +0000

    +

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
 # import color
-import math
-a,b,c = float(input()),float(input()),float(input())
-D = b**2-4*a*c
-x = (((-b+math.sqrt(D))/2*a),((-b-math.sqrt(D))/2*a))
-if D > 0:
-    print(min(x))
-    print(max(x))        
-elif D == 0:
-    print(-b/2*a)
-else:
-    print("Нет корней")
+
+print(19 % 10)
