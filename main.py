cd47ac95d2a94108c7787b1fac4c046131cd207b
SADfranco <76488291+SADfranco@users.noreply.github.com>
2022-05-31 15:56:42 +0000

    +

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 # import color
+num = int(input())
+total = 0
+while num != 0:
+    if num >= 25:
+        num -= 25
+        total += 1
+    if 10 <= num < 25:
+        num -= 10
+        total += 1
+    if 5 <= num < 10:
+        num -= 5
+        total += 1   
+    if 1 <= num < 5:
+        num -= 1
+        total += 1        
 
-print(19 % 10)
+print(total)
