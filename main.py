f902dca781548c7998d219303e77704ef09632c2
SADfranco <76488291+SADfranco@users.noreply.github.com>
2022-06-02 15:21:59 +0000

    +

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-# import color
-num = int(input())
+n = int(input())
+i = n % 10
 total = 0
-while num != 0:
-    if num >= 25:
-        num -= 25
+while n > 0:
+    num = n % 10
+    if i != num:
         total += 1
-    if 10 <= num < 25:
-        num -= 10
-        total += 1
-    if 5 <= num < 10:
-        num -= 5
-        total += 1   
-    if 1 <= num < 5:
-        num -= 1
-        total += 1        
-
-print(total)
+        print('NO')
+    n = n // 10
+if total == 0:
+    print('YES')
